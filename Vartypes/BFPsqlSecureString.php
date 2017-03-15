64e30037e18f71e0970b35d34036889f04a5b9b0
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2017-03-15 08:27:16 +0100

    New BFPsqlSecureString type

---
+++ Vartypes/BFPsqlSecureString.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * GNU General Public License (Version 2, June 1991)
+ *
+ * This program is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free
+ * Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+namespace Beeflow\SQLQueryManager\Vartypes;
+
+/**
+ * Class BFPsqlSecureString
+ *
+ * @author  Rafal Przetakowski <rafal.p@beeflow.co.uk>
+ * @package Beeflow\SQLQueryManager\Vartypes
+ */
+class BFPsqlSecureString
+{
+    /**
+     * Wartość zmienej
+     *
+     * @var Mixed
+     */
+    private $value;
+
+    /**
+     * BFPsqlSecureString constructor.
+     *
+     * @param $value
+     *
+     * @throws \Exception
+     */
+    public function __construct($value)
+    {
+        $value = strtr(strip_tags($value),
+        array(
+            "'"    => "''",
+            "\0"   => "",
+            "--"   => "",
+            ");"   => "",
+            ")"    => "",
+            "}"    => "",
+            "("    => "",
+            "("    => "",
+            "<!--" => "",
+            "<"    => "&lt;",
+            ">"    => "&gt;"
+            // more secure
+        ));
+
+        if (gettype($value) == 'string') {
+            $this->value = $value;
+        } else {
+            throw new \Exception('Value must be ' . __CLASS__ . ' type but is ' . gettype($value));
+        }
+    }
+
+    /**
+     * @return String
+     */
+    public function val()
+    {
+        return $this->__toString();
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->value;
+    }
+}
